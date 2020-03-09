65b698c5e00bcec4218649534debc9664335fbe4
tthebst <gretler.tim@gmail.com>
2020-03-09 21:13:35 +0100

    some small changes

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module api-test
 
 go 1.13
 
-require github.com/gorilla/mux v1.7.4
+require (
+	github.com/gorilla/mux v1.7.4
+	github.com/sjwhitworth/golearn v0.0.0-20191230161259-3e43e74895fb // indirect
+	github.com/smartystreets/goconvey v1.6.4 // indirect
+	gonum.org/v1/gonum v0.7.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,69 +1,73 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
 	"encoding/json"
+	"log"
+	"net/http"
 
-    "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
 
 func get(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(http.StatusOK)
-    w.Write([]byte(`{"message": "get called"}`))
+	http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+	return
 }
 
-
 type data_struct struct {
-    X_data []int
-    Y_data []int
+	X_data []int
+	Y_data []int
 }
 
 func post(w http.ResponseWriter, r *http.Request) {
-    
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(http.StatusCreated)
-    r.ParseForm()
-    log.Println(r.Form)
-    var d data_struct
 
-    // Try to decode the request body into the struct. If there is an error,
-    // respond to the client with the error message and a 400 status code.
-    err := json.NewDecoder(r.Body).Decode(&d)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusBadRequest)
-        return
-    }
-    fmt.Printf("%v", d.X_data)
-    fmt.Printf("%v", d.Y_data)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	r.ParseForm()
+	log.Println(r.Form)
+	var d data_struct
 
+	// Try to decode the request body into the struct. If there is an error,
+	// respond to the client with the error message and a 400 status code.
+	err := json.NewDecoder(r.Body).Decode(&d)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	// Error if provided x_data array contains no values
+	if len(d.X_data) == 0 {
+		http.Error(w, "please provide x_data", http.StatusBadRequest)
+		return
 
-    w.Write([]byte(`{"message": "post called"}`))
+	}
+	// Error if provided y_data array contains no values
+	if len(d.Y_data) == 0 {
+		http.Error(w, "please provide y_data", http.StatusBadRequest)
+		return
+
+	}
+	var svm = NewLinearSVC("l2", "l2", true, 1.0, 0.2)(*LinearSVC, error)
+
+	w.Write([]byte(`{"message": "post called"}`))
 }
 
 func put(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(http.StatusAccepted)
-    w.Write([]byte(`{"message": "put called"}`))
+	http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+	return
 }
 
 func delete(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(http.StatusOK)
-    w.Write([]byte(`{"message": "delete called"}`))
+	http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+	return
 }
 
-
 func main() {
-    r := mux.NewRouter()
+	r := mux.NewRouter()
 
-    api := r.PathPrefix("/api/v1").Subrouter()
-    api.HandleFunc("", get).Methods(http.MethodGet)
-    api.HandleFunc("", post).Methods(http.MethodPost)
-    api.HandleFunc("", put).Methods(http.MethodPut)
-    api.HandleFunc("", delete).Methods(http.MethodDelete)
+	api := r.PathPrefix("/api/v1").Subrouter()
+	api.HandleFunc("", get).Methods(http.MethodGet)
+	api.HandleFunc("", post).Methods(http.MethodPost)
+	api.HandleFunc("", put).Methods(http.MethodPut)
+	api.HandleFunc("", delete).Methods(http.MethodDelete)
 
-    log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
